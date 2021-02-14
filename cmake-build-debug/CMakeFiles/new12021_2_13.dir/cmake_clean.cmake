be66fac2b1909c029c9fee8c09d9272a361e7bf5
Lee <2362313731@qq.com>
2021-02-14 09:12:04 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/new12021_2_13.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/new12021_2_13.dir/complex.cpp.obj"
+  "libnew12021_2_13.dll.a"
+  "new12021_2_13.exe"
+  "new12021_2_13.exe.manifest"
+  "new12021_2_13.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/new12021_2_13.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ complex.h
@@ -0,0 +1,19 @@
+//
+// Created by 23623 on 2021/2/14.
+//
+
+#ifndef NEW12021_2_13_COMPLEX_H
+#define NEW12021_2_13_COMPLEX_H
+
+//下面是前置声明
+class ostream;
+class complex;
+
+complex&
+    __doapl (complex* ths,const complex& r);
+
+//下面是类的声明
+class complex{
+
+};
+#endif //NEW12021_2_13_COMPLEX_H
